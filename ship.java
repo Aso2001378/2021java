d589346002412313af6fbaaf52f992245aa4889f
Aso2001378 <83046677+Aso2001378@users.noreply.github.com>
2021-07-30 18:21:26 +0900

    Update ship.java

--- ship.java
+++ ship.java
@@ -2,5 +2,15 @@ package saisyuu;
 
 public class Ship {
     
-    
+private int x;
+private int y;
+
+public final int getx() {
+    return x;
+
+public final int gety() {
+    return y;
+    System.out.print(x+y);
+    }
 }
+
